407c91a2a05f082faf01b7a18bcc107b80ae5e15
Joan Pareja <joanparejapinyol@gmail.com>
2016-04-11 11:58:23 +0200

    Version 0.1 preparation

---
+++ OutZone/ModuleBackground.cpp
@@ -0,0 +1,34 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleBackground.h"
+
+
+ModuleBackground::ModuleBackground()
+{
+	background.x = 0;
+	background.y = 0;
+	background.w = 240;
+	background.h = 3896;
+}
+
+ModuleBackground::~ModuleBackground()
+{}
+
+// Load assets
+bool ModuleBackground::Start()
+{
+	LOG("Loading background assets");
+	bool ret = true;
+	graphics = App->textures->Load("level1.png");
+	return ret;
+}
+
+// Update: draw background
+update_status ModuleBackground::Update()
+{
+	App->render->Blit(graphics, 0, -3576, &background, 0.75f);
+
+	return UPDATE_CONTINUE;
+}
