eaa2a77ba607657ef81a40ef3c46f8495d0696a9
JoanPareja <joanparejapinyol@gmail.com>
2016-04-11 16:47:52 +0200

    Replaced bakcroung img with the correct one

--- OutZone/ModuleBackground.cpp
+++ OutZone/ModuleBackground.cpp
@@ -7,10 +7,10 @@
 
 ModuleBackground::ModuleBackground()
 {
-	background.x = 0;
+	background.x = 1;
 	background.y = 0;
 	background.w = 240;
-	background.h = 3896;
+	background.h = 4756;
 }
 
 ModuleBackground::~ModuleBackground()
@@ -21,14 +21,14 @@ bool ModuleBackground::Start()
 {
 	LOG("Loading background assets");
 	bool ret = true;
-	graphics = App->textures->Load("Game/level1.png");
+	graphics = App->textures->Load("Game/lvl_1.png");
 	return ret;
 }
 
 // Update: draw background
 update_status ModuleBackground::Update()
 {
-	App->render->Blit(graphics, 0, -3576, &background, 0.75f);
+	App->render->Blit(graphics, 0, -4756 + SCREEN_HEIGHT, &background, 0.75f); //Negative value to start rendering from the bottom og the image
 
 	return UPDATE_CONTINUE;
 }
