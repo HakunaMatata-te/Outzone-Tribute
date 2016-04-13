bedd288cf3ab73a9fd09b05927dc24be19cd956e
JoanPareja <joanparejapinyol@gmail.com>
2016-04-13 23:16:36 +0200

    Added Particles (Just to load, no gamplay functionality)

---
+++ OutZone/ModuleParticles.cpp
@@ -0,0 +1,123 @@
+#include <math.h>
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleParticles.h"
+
+#include "SDL/include/SDL_timer.h"
+
+ModuleParticles::ModuleParticles()
+{
+	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+		active[i] = nullptr;
+}
+
+ModuleParticles::~ModuleParticles()
+{}
+
+// Load assets
+bool ModuleParticles::Start()
+{
+	LOG("Loading particles");
+	particles_texture = App->textures->Load("weapon_shots.png");
+
+	// Minigun particle
+	minigun_shot_lv1.anim.PushBack({ 55, 244, 4, 16 });
+	minigun_shot_lv1.speed.y = 5;
+
+	// Triple shot particle
+	triple_shot_lv1.anim.PushBack({ 137, 246, 6, 14 });
+	minigun_shot_lv1.speed.y = 5;
+
+	return true;
+}
+
+// Unload assets
+bool ModuleParticles::CleanUp()
+{
+	LOG("Unloading particles");
+	App->textures->Unload(particles_texture);
+
+	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		if (active[i] != nullptr)
+		{
+			delete active[i];
+			active[i] = nullptr;
+		}
+	}
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleParticles::Update()
+{
+	for (uint i = 0; i < MAX_ACTIVE_PARTICLES; ++i)
+	{
+		Particle* p = active[i];
+
+		if (p == nullptr)
+			continue;
+
+		if (p->Update() == false)
+		{
+			delete p;
+			active[i] = nullptr;
+		}
+		else if (SDL_GetTicks() >= p->born)
+		{
+			App->render->Blit(particles_texture, p->position.x, p->position.y, &(p->anim.GetCurrentFrame()));
+			if (p->fx_played == false)
+			{
+				p->fx_played = true;
+				// Play particle fx here
+			}
+		}
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleParticles::AddParticle(const Particle& particle, int x, int y, Uint32 delay)
+{
+	Particle* p = new Particle(particle);
+	p->born = SDL_GetTicks() + delay;
+	p->position.x = x;
+	p->position.y = y;
+
+	active[last_particle++] = p;
+}
+
+// Particle sruct methods-------------------------------------------------------------
+
+Particle::Particle()
+{
+	position.SetToZero();
+	speed.SetToZero();
+}
+
+Particle::Particle(const Particle& p) :
+anim(p.anim), position(p.position), speed(p.speed),
+fx(p.fx), born(p.born), life(p.life)
+{}
+
+bool Particle::Update()
+{
+	bool ret = true;
+
+	if (life > 0)
+	{
+		if ((SDL_GetTicks() - born) > life)
+			ret = false;
+	}
+	else
+		if (anim.Finished())
+			ret = false;
+
+	position.x += speed.x;
+	position.y += speed.y;
+
+	return ret;
+}
